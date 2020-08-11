28580b115ecd85c1a127a6294149973c68def8c3
Dean Wilson <dean.wilson@gmail.com>
2020-08-11 18:01:36 +0100

    Fix rubocop issues

--- spec/rpv/process_spec.rb
+++ spec/rpv/process_spec.rb
@@ -1,32 +1,31 @@
 require 'rpv/process'
 require 'rpv/filter'
 
-describe "Rpv::Process" do
+describe 'Rpv::Process' do
+  it 'returnd the pid value used in the constructor' do
+    p = Rpv::Process.new(30, 1, 'root', '/usr/sbin/testy')
 
-  it "should return the values used in the constructor" do
-    p = Rpv::Process.new(30, 1, "root", "/usr/sbin/testy")
-    p.pid.should  == 30
-    p.ppid.should ==  1
+    expect(p.pid).to eq(30)
+    expect(p.ppid).to eq(1)
 
-    p.uname.should == "root"
-    p.command.should == "/usr/sbin/testy"
+    p.uname.should == 'root'
+    p.command.should == '/usr/sbin/testy'
   end
 
-  it "should return a pretty string" do
-    p = Rpv::Process.new(30, 1, "root", "/usr/sbin/testy")
-    p.to_s.should == "Pid: 30, ppid: 1, uname: root command: /usr/sbin/testy"
+  it 'returns a string representation' do
+    p = Rpv::Process.new(30, 1, 'root', '/usr/sbin/testy')
+    p.to_s.should == 'Pid: 30, ppid: 1, uname: root command: /usr/sbin/testy'
   end
 
-  it "should match on a correct filter" do
-    p = Rpv::Process.new(30, 1, "root", "/usr/sbin/testy")
-    f = Rpv::Filter.new "uname => root, command => /usr/sbin/testy"
+  it 'matches on a correct filter' do
+    p = Rpv::Process.new(30, 1, 'root', '/usr/sbin/testy')
+    f = Rpv::Filter.new 'uname => root, command => /usr/sbin/testy'
     p.matches?(f).should be_true
   end
 
-  it "shouldn't match on an incorrect filter" do
-    p = Rpv::Process.new(30, 1, "root", "/usr/sbin/testy")
-    f = Rpv::Filter.new "uname => testy, command => /usr/local/bin/testy"
+  it 'does not match on an incorrect filter' do
+    p = Rpv::Process.new(30, 1, 'root', '/usr/sbin/testy')
+    f = Rpv::Filter.new 'uname => testy, command => /usr/local/bin/testy'
     p.matches?(f).should be_false
   end
-
 end
