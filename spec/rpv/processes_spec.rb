91ee0bfa9e400432bc44853a44e5c6f1d77e03fe
Dean Wilson <dean.wilson@gmail.com>
2020-08-11 18:04:22 +0100

    Fix rubocop issues

--- spec/rpv/processes_spec.rb
+++ spec/rpv/processes_spec.rb
@@ -1,20 +1,19 @@
 require 'rpv/processes'
 
-describe "Rpv::Processes" do
-
-  it "should return the fields to use in ps" do
+describe 'Rpv::Processes' do
+  it 'returns the fields to use in ps' do
     p = Rpv::Processes.new
 
     p.fields.length.should == 4
-    p.fields.include?("command").should be_true
-    p.fields.include?("uname").should be_true
+    p.fields.include?('command').should be_true
+    p.fields.include?('uname').should be_true
   end
 
-  it "should extract a simple ps structure" do
+  it 'extracts a simple ps structure' do
     process = 'root      2898     1 syslogd -m 0'
 
     p = Rpv::Processes.new
-    ds = p.extract( p.fields, process)
+    ds = p.extract(p.fields, process)
 
     ds.keys.length.should == 4
 
@@ -25,19 +24,17 @@ describe "Rpv::Processes" do
     ds['uname'].should == 'root'
   end
 
-  it "should load processes from the system" do
-
+  it 'loads processes from the system' do
   end
 
-  it "should contain processes" do
-    # TODO stub this
+  it 'contains processes' do
+    # TODO: stub this
     p = Rpv::Processes.new
     p.count.should >= 100
   end
 
-  it "should return an array of process objects when asked" do
+  it 'returns an array of process objects when asked' do
     p = Rpv::Processes.new
     p.processes.class.should == Array
   end
-
 end
