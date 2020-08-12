d433852577f616ba2c985fc54965e32418d5c8c8
Dean Wilson <dean.wilson@gmail.com>
2020-08-12 09:04:32 +0100

    Update tests to use new rspec formats

--- spec/rpv/filter_spec.rb
+++ spec/rpv/filter_spec.rb
@@ -5,27 +5,37 @@ describe 'Rpv::Filter' do
     line = 'uname => root, command => /usr/bin/bash'
 
     f = Rpv::Filter.new line
-    f.split(line).length.should == 2
+    expect(f.split(line).length).to eq 2
   end
 
   # TODO: add broken cases
-  it 'extracts pairs' do
+  context 'when provided multiple correct pairs' do
     line = 'uname => root, command => /usr/bin/bash'
 
     f = Rpv::Filter.new line
 
-    [ %w[uname root], ['command', '  /usr/bin/bash arg ']].each do |pair|
+    [%w[uname root], ['command', '  /usr/bin/bash arg ']].each do |pair|
       extracted = f.extract_pair("#{pair[0]} => #{pair[1]}")
 
-      extracted.keys.length.should   == 1
-      extracted.values.length.should == 1
+      it "extracts a single #{pair[0]} key" do
+        expect(extracted.keys.length).to eq 1
+      end
 
-      extracted.keys[0].should   == pair[0].strip
-      extracted.values[0].should == pair[1].strip
+      it "extracts a single #{pair[1]} value" do
+        expect(extracted.values.length).to eq 1
+      end
+
+      it "matches [#{pair[0]}] as the key name" do
+        expect(extracted.keys[0]).to eq pair[0].strip
+      end
+
+      it "matches [#{pair[1]}] as the key value" do
+        expect(extracted.values[0]).to eq pair[1].strip
+      end
     end
   end
-
-  it 'extracts fields' do
-    filter = 'uname => root, command =>    /usr/bin/bash arg'
-  end
+  # TODO: implement this test
+  #  it 'extracts fields' do
+  #    filter = 'uname => root, command =>    /usr/bin/bash arg'
+  #  end
 end
